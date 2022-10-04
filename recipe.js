690716449b34d6d14af09ff4141516326549be38
Asdren JERLIU <114486010+asdreen@users.noreply.github.com>
2022-10-04 16:16:32 +0200

    Add files via upload

---
+++ recipe.js
@@ -0,0 +1,75 @@
+// My FIRST JS LINES!
+
+let firstName = "Asdren";
+let lastName = "Jerliu";
+let space = " ";
+
+let Hello,
+  MyName = firstName + space + lastName;
+console.log("Hello, MyName is: ", MyName);
+
+let firstNumber = 34;
+let secondNumber = 2022;
+
+let numbersCalculated = secondNumber - firstNumber;
+
+console.log("numbersCalculated is: ", numbersCalculated);
+
+let lastResume = MyName + space + numbersCalculated;
+console.log("lastResume is: ", lastResume);
+
+
+// LET`S COOK SOME PASTA!
+
+
+// INPUTS:
+let spaghetti = 400
+let guanciale = 250
+let eggYolks = 6
+let cheese = 50
+let pepper = 4
+
+//OUTPUTS:
+console.log('spaghetti 400g', spaghetti)
+console.log('guanciale 250g', guanciale)
+console.log('eggYolks 6pc', eggYolks)
+console.log('cheese 50g', cheese)
+console.log('pepper 4g', pepper)
+
+console.log('.....start cooking Carbonara.....')
+
+let guancialeslides = 1
+console.log('cut Guanciala Slides 1cm', guancialeslides)
+
+let firstStep = eggYolks + cheese
+console.log('mix the cheese and eggs')
+
+let secondStep = firstStep + pepper
+console.log('Roast the black pepper on a pan until it smokes and sizzles, and combine a small amount of it to the yolks and chees mixture')
+
+let browngua = 3
+console.log('brown Gua In Medium Heat 3min', browngua)
+let greengua = 1
+console.log('green Gua In Max Heat 1min', greengua)
+
+let water = 3000
+console.log('water 3l', water)
+let salt =1
+console.log('salt 1g', salt)
+
+let cookePasta = water + spaghetti + salt
+console.log('Cook the pasta with a pinch of salt in the water', cookePasta)
+
+let drainedPasta = spaghetti - water
+console.log('set aside a ladleful of the pasta water before draining it,then drain the pasta once it is cooked al dente', drainedPasta)
+let makeSauce = guanciale + water
+console.log('2 spoons of the hot water into the part of the frying pan containing the cooled guanciale and turn on the heat', makeSauce)
+
+let thirdStep = makeSauce + drainedPasta
+console.log('put in one pan sauce and pasta and mix it', thirdStep)
+
+let fourthStep = thirdStep + firstStep
+console.log('mix all together', fourthStep)
+
+let CarbonaraReady = fourthStep + secondStep
+console.log('Add the remaining roasted black pepper and serve immediately', CarbonaraReady)
